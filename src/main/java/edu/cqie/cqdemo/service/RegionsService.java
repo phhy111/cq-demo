54e7b3c4ddba6991c1b121d01911c6a58d7032b9
chen-rgb-hl <1735597076@qq.com>
2026-02-01 09:32:21 +0800

    slideShowAndRoutes

---
+++ src/main/java/edu/cqie/cqdemo/service/RegionsService.java
@@ -0,0 +1,19 @@
+package edu.cqie.cqdemo.service;
+
+import edu.cqie.cqdemo.entity.Regions;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
+
+/**
+* @author
+* @description 针对表【regions(区域表)】的数据库操作Service
+* @createDate 2026-01-31 21:31:23
+*/
+public interface RegionsService extends IService<Regions> {
+    /**
+     * 获取区域id与区域名称
+     * @return
+     */
+    public List<Regions> getRegionsIdAndName();
+}
