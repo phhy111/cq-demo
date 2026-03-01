bd4a4a823c89fb7011c13851f69e0fe359d43519
phhy111 <3107996429@qq.com>
2026-03-01 20:15:40 +0800

    初版

--- src/main/java/edu/cqie/cqdemo/service/impl/FoodCategoriesServiceImpl.java
+++ src/main/java/edu/cqie/cqdemo/service/impl/FoodCategoriesServiceImpl.java
@@ -4,21 +4,14 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import edu.cqie.cqdemo.entity.FoodCategories;
 import edu.cqie.cqdemo.mapper.FoodCategoriesMapper;
 import edu.cqie.cqdemo.service.FoodCategoriesService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class FoodCategoriesServiceImpl extends ServiceImpl<FoodCategoriesMapper, FoodCategories> implements FoodCategoriesService {
-
-    @Autowired
-    private FoodCategoriesMapper foodCategoriesMapper;
-
-    public List<FoodCategories> getAllStatus()
-    {
-        List<FoodCategories> foodCategoriesList=foodCategoriesMapper.getAllStatus();
-
-        return foodCategoriesList;
+    @Override
+    public List<FoodCategories> getAllStatus() {
+        return list();
     }
 }
